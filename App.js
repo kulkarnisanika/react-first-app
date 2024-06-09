36fdb359bebe87a0ff3b3142e4b549caf8a1aa86
Sanika Kulkarni <sanika.kulkarni@amdocs.com>
2024-06-09 17:20:52 +0530

    first checkin

---
+++ App.js
@@ -0,0 +1,10 @@
+//It will hold all react code
+
+ //create HTML element by React
+ const heading = React.createElement("h1", {"id": "heading"} , "Hello React!!!");
+
+ //find element and create it in react
+ const findEle = ReactDOM.createRoot(document.getElementById("main"));
+
+ //add created element in created  DOM which was found before step
+ findEle.render(heading);
